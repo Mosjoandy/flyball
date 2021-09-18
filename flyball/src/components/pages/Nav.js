e14f2c0d4868fca9b316f1e09b8f584e03915531
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2021-09-17 19:15:00 -0700

    converted nav into component, added hide scroll up

--- flyball/src/components/pages/Nav.js
+++ flyball/src/components/pages/Nav.js
@@ -1,51 +1,88 @@
-import React from 'react';
-// import './Nav.css';
+import React, { Component } from 'react';
+import './Nav.css';
 import { Navbar, Container, NavDropdown, Image } from "react-bootstrap";
 import Instagram from "../../images/instagram.png";
 import Facebook from "../../images/facebook.png";
-
-
-function Nav() {
-    return (
-        <Container >
-            <Navbar fixed="top" style={{background:'#008000'}} >
-                <Container>
-                    <Navbar.Collapse id="socialIconRow" className="d-flex justify-content-between" >
-
-                        <Navbar.Brand>
-                            <a href="https://www.instagram.com/theusualsuspectsfc/">
-                                <Image
-                                    src={Instagram}
-                                    style={{ height: '30px', width: '30px', margin: '0 10px' }}
-                                />
-                            </a>
-                            <a href="https://www.facebook.com/pg/theusualsuspectsfc/">
-                                <Image
-                                    src={Facebook}
-                                    style={{ height: '30px', width: '30px' }}
-                                />
-                            </a>
-                        </Navbar.Brand>
-                        <NavDropdown
-                            title="Navigation"
-                      
-                        >
-                            <NavDropdown.Item href="" style={{ color: '#008000' }}>About Us</NavDropdown.Item>
-                            <NavDropdown.Item href="" style={{ color: '#008000' }}>Activities</NavDropdown.Item>
-                            <NavDropdown.Item href="" style={{ color: '#008000' }}>The Team</NavDropdown.Item>
-                            <NavDropdown.Item href="" style={{ color: '#008000' }}>Contact</NavDropdown.Item>
-
-                        </NavDropdown>
+import classnames from "classnames";
 
 
 
+class Nav extends Component {
+    constructor(props) {
+        super(props);
 
-                    </Navbar.Collapse>
+        this.state = {
+            prevScrollpos: window.pageYOffset,
+            visible: true
+        };
+    };
 
-                </Container>
-            </Navbar>
-        </Container>
-    );
+    componentDidMount() {
+        window.addEventListener("scroll", this.handleScroll);
+    };
+
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.handleScroll);
+    };
+
+
+    handleScroll = () => {
+        const { prevScrollpos } = this.state;
+
+        const currentScrollPos = window.pageYOffset;
+        const visible = prevScrollpos > currentScrollPos;
+
+        this.setState({
+            prevScrollpos: currentScrollPos,
+            visible
+        });
+    };
+
+    render() {
+        return (
+            <Container className={classnames("navUp", {
+                "navUp--hidden": !this.state.visible
+            })}>
+                <Navbar style={{ background: '#008000' }} >
+                    <Container>
+                        <Navbar.Collapse id="socialIconRow" className="d-flex justify-content-between" >
+
+                            <Navbar.Brand>
+                                <a href="https://www.instagram.com/theusualsuspectsfc/">
+                                    <Image
+                                        src={Instagram}
+                                        style={{ height: '30px', width: '30px', margin: '0 10px' }}
+                                    />
+                                </a>
+                                <a href="https://www.facebook.com/pg/theusualsuspectsfc/">
+                                    <Image
+                                        src={Facebook}
+                                        style={{ height: '30px', width: '30px' }}
+                                    />
+                                </a>
+                            </Navbar.Brand>
+                            <NavDropdown
+                                title="Navigation"
+
+                            >
+                                <NavDropdown.Item href="" style={{ color: '#008000' }}>About Us</NavDropdown.Item>
+                                <NavDropdown.Item href="" style={{ color: '#008000' }}>Activities</NavDropdown.Item>
+                                <NavDropdown.Item href="" style={{ color: '#008000' }}>The Team</NavDropdown.Item>
+                                <NavDropdown.Item href="" style={{ color: '#008000' }}>Contact</NavDropdown.Item>
+
+                            </NavDropdown>
+
+
+
+
+                        </Navbar.Collapse>
+
+                    </Container>
+                </Navbar>
+            </Container>
+        );
+    };
+
 };
 
-export default Nav
+export default Nav;
