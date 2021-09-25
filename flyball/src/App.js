888096682e8b10167ee3eb6a128191965b5c15df
ItzDeme <61122692+ItzDeme@users.noreply.github.com>
2021-09-25 13:51:43 -0700

    added font mistral, added media queries
    created a class name "mistral-font", use for when ever you want to use mistral font.
    
    created media queries to not have that blank space for mobile devices. The queries were added to about/what panels. AboutPanel may need some tweaking.

--- flyball/src/App.js
+++ flyball/src/App.js
@@ -1,32 +1,27 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 
-import Nav from './components/pages/Nav.js'
-import TopPanel from './components/pages/TopPanel.js'
-import WhatPanel from './components/pages/WhatPanel.js'
-import AboutUsPanel from './components/pages/AboutUsPanel.js'
-import ScheduleTitle from './components/pages/ScheduleTitle.js'
-import SchedulePanel from './components/pages/SchedulePanel.js'
-import CompetitionPanel from './components/pages/CompetitionPanel.js'
+import Nav from "./components/pages/Nav.js";
+import TopPanel from "./components/pages/TopPanel.js";
+import WhatPanel from "./components/pages/WhatPanel.js";
+import AboutUsPanel from "./components/pages/AboutUsPanel.js";
+import ScheduleTitle from "./components/pages/ScheduleTitle.js";
+import SchedulePanel from "./components/pages/SchedulePanel.js";
+import CompetitionPanel from "./components/pages/CompetitionPanel.js";
 import Contact from "./components/pages/Contact";
 import TheTeam from "./components/pages/TheTeam";
 // import * as Page from './components/pages';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 // import { render } from '@testing-library/react';
 
-
-
 function App() {
-
   return (
     <div>
       <Router>
         <div>
           <Route exact path="/">
-
-           
             <Nav />
             <TopPanel />
             <WhatPanel />
@@ -41,13 +36,9 @@ function App() {
       </Router>
     </div>
   );
-};
-
-
-
+}
 
 export default App;
-
 
 // class App extends Component {
 //   constructor() {
