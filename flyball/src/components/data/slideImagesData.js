1e101a6ab3fb375e624a102c582f152ba47a5a7e
ItzDeme <61122692+ItzDeme@users.noreply.github.com>
2021-10-02 18:44:08 -0700

    Added Slide Show
    Added slide show, npm install before doing anything. Changed the names of the slideshow images. Added a folder for images. Deleted the useless constructor in TheTeam.js

---
+++ flyball/src/components/data/slideImagesData.js
@@ -0,0 +1,25 @@
+import image1 from "../../images/slideImage1.jpg";
+import image2 from "../../images/slideImage2.jpg";
+import image3 from "../../images/slideImage3.jpg";
+import image4 from "../../images/slideImage4.jpg";
+
+const imageData = [
+  {
+    original: image1,
+    thumbnail: image1,
+  },
+  {
+    original: image2,
+    thumbnail: image2,
+  },
+  {
+    original: image3,
+    thumbnail: image3,
+  },
+  {
+    original: image4,
+    thumbnail: image4,
+  },
+];
+
+export default imageData;
--- flyball/src/components/pages/TheTeam.js
+++ flyball/src/components/pages/TheTeam.js
@@ -1,33 +1,50 @@
-import React, { Component } from 'react';
-import './TheTeam.css';
+import React, { Component } from "react";
+import ImageGallery from "react-image-gallery";
+import "./TheTeam.css";
+import imageData from "../data/slideImagesData";
 import { Container, Row, Col, Card } from "react-bootstrap";
 
 class TheTeam extends Component {
-    constructor(props) {
-        super(props)
-    }
+  render() {
+    const rngSlideStart = Math.floor(Math.random() * 4);
 
-    render() {
-        return (
-            <Container fluid id='theTeam'>
-                <Row>
-                    <Col>
-                        <Card.Text className="text-center aboutUsTitle negative-margin mistral-font">
-                            The Team
-                        </Card.Text>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Card.Title className="text-center" style={{ fontWeight: '600', color: '#008000', paddingBottom: '3vh' }}>
-                            Coming Soon!
-                        </Card.Title>
-                    </Col>
-                </Row>
-            </Container>
-        )
-    }
-};
+    return (
+      <Container fluid id="theTeam">
+        <Row>
+          <Col>
+            <Card.Text className="text-center aboutUsTitle negative-margin mistral-font">
+              The Team
+            </Card.Text>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Card.Title
+              className="text-center"
+              style={{
+                fontWeight: "600",
+                color: "#008000",
+                paddingBottom: "3vh",
+              }}
+            >
+              Coming Soon!
+            </Card.Title>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <ImageGallery
+              showPlayButton={false}
+              autoPlay={true}
+              startIndex={rngSlideStart}
+              showFullscreenButton={false}
+              items={imageData}
+            />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
 
-export default TheTeam
-
+export default TheTeam;
