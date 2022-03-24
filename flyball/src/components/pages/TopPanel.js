218cc5b2cfb603a52abf5f5862ce5545e7ca70b2
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2022-03-23 23:11:05 -0700

    added new logo

--- flyball/src/components/pages/TopPanel.js
+++ flyball/src/components/pages/TopPanel.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import './TopPanel.css';
 import Logo1 from "../../images/Logo1.png";
 import Logo2 from "../../images/Logo2.png";
+import LineUpLogo from "../../images/UsualSuspects_LineUp.png"
 import { Container, Row, Col, Card, Image } from "react-bootstrap";
 
 class TopPanel extends Component {
@@ -14,19 +15,19 @@ class TopPanel extends Component {
     };
 
     render() {
-        const hijutsu = Math.floor(Math.random() * 2)
+        // const hijutsu = Math.floor(Math.random() * 2)
         return (
             <Container fluid>
                 <Row>
                     <Col sm={12} xl>
 
-                        <Image src={this.state.Logos[hijutsu]}
+                        <Image src={LineUpLogo}
                             style={{
-                                width: '35%',
+                                width: '100%',
                                 marginLeft: 'auto',
                                 marginRight: 'auto',
                                 display: 'block',
-                                paddingTop: '9vh'
+                                paddingTop: '11vh'
                             }} />
                         <Card.Body className="text-center">
                             <Card.Title style={{ color: '#008000' }}>
